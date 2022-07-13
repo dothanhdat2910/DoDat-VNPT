f1cedadccf35982bb82cbb420ec668f910cf5508
dothanhdat2910 <106945207+dothanhdat2910@users.noreply.github.com>
2022-07-13 18:08:35 +0700

    update Flask-Mail

--- Flask/mail.py
+++ Flask/mail.py
@@ -1,19 +1,30 @@
-from crypt import methods
-from re import T
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask_mail import Mail, Message
-import os
+
+
 app = Flask(__name__)
-app.config['MAIL_SERVER'] = 'gmail.com'
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = 'dothanhdatk18@gmail.com'
-app.config['MAIL_PASSWORD'] = os.environ.get('PASSWORD')
+app.config['MAIL_PASSWORD'] = 'vkrosjgcepfboezc'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
+
 mail = Mail(app)
 
-@app.route('/home',methods = ['GET','POST'])
-@app.route('/',methods = ['GET','POST'])
+
+
+@app.route('/home', methods= ['GET', 'POST'])
+@app.route('/', methods= ['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        msg = Message('Hey',sender='')
+        msg = Message("Nhiệm vụ thứ 4", sender='dothanhdatk18@gmail.com', recipients=['vutruong5438@gmail.com', 'thopn95@gmail.com'])
+        msg.body = "Hello 2 a, e nộp bài gửi mail"
+        mail.send(msg)
+        return "Sent email"
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
