0bdfe5ea8987d14ace149bc2111e5fb5a515687a
dothanhdat2910 <106945207+dothanhdat2910@users.noreply.github.com>
2022-07-15 11:05:43 +0700

    Delete SQLlite directory

--- SQLlite/delete_table.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('member.db')
-cursor= conn.cursor()
-st_delete= "DELETE FROM STUDENT WHERE NAME ='Hieu' "
-cursor.execute(st_delete)
-conn.commit()
-conn.close()
